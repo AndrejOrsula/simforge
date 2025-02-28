8d5c9244d58c685d8c01fb0b11cff1ada1964e18
Andrej Orsula <orsula.andrej@gmail.com>
2025-02-28 19:00:31 +0100

    isaaclab: Refactor USD API handling
    Signed-off-by: Andrej Orsula <orsula.andrej@gmail.com>

--- simforge/integrations/isaaclab/schemas/impl.py
+++ simforge/integrations/isaaclab/schemas/impl.py
@@ -1,7 +1,6 @@
 from typing import TYPE_CHECKING
 
 import carb
-from isaaclab.sim import apply_nested
 from isaacsim.core.utils.stage import get_current_stage
 from omni.physx.scripts import utils as physx_utils
 from pxr import Usd, UsdPhysics
@@ -12,10 +11,8 @@ if TYPE_CHECKING:
     from simforge.integrations.isaaclab.schemas.cfg import MeshCollisionPropertiesCfg
 
 
-@apply_nested
 def set_mesh_collision_properties(
-    prim_path: str,
-    cfg: "MeshCollisionPropertiesCfg",
+    prim_path: str, cfg: "MeshCollisionPropertiesCfg"
 ) -> bool:
     """
 
@@ -27,46 +24,40 @@ def set_mesh_collision_properties(
         True if the properties were successfully set, False otherwise.
     """
 
-    # Apply mesh collision approximation
-    if cfg.mesh_approximation is not None:
-        prim: Usd.Prim = get_current_stage().GetPrimAtPath(prim_path)
+    if cfg.mesh_approximation is None:
+        return True
 
-        if physx_utils.hasSchema(prim, "CollisionAPI"):
-            carb.log_warn("CollisionAPI is already defined")
-            return False
+    parent_prim: Usd.Prim = get_current_stage().GetPrimAtPath(prim_path)
+    _queue = [parent_prim]
+    while _queue:
+        child_prim = _queue.pop(0)
+        _queue.extend(child_prim.GetChildren())
+        if not child_prim.HasAPI(UsdPhysics.CollisionAPI):
+            continue
 
-        def isPartOfRigidBody(currPrim):
-            if currPrim.HasAPI(UsdPhysics.RigidBodyAPI):  # type: ignore
-                return True
-
-            currPrim = currPrim.GetParent()
-
-            return isPartOfRigidBody(currPrim) if currPrim.IsValid() else False
-
-        if cfg.mesh_approximation == "none" and isPartOfRigidBody(prim):
+        if cfg.mesh_approximation == "none" and is_part_of_rigid_body(child_prim):
             carb.log_warn(
-                f"setCollider: {prim.GetPath()} is a part of a rigid body. Resetting approximation shape from none (trimesh) to convexHull"
+                f'Prim "{child_prim.GetPath()}" is a part of a rigid body, so its collision mesh should be approximated (mesh_approximation="none")'
             )
-            cfg.mesh_approximation = "convexHull"
 
-        collisionAPI = UsdPhysics.CollisionAPI.Apply(prim)  # type: ignore
-        PhysxSchema.PhysxCollisionAPI.Apply(prim)
-        collisionAPI.CreateCollisionEnabledAttr().Set(True)
+        if not child_prim.HasAPI(PhysxSchema.PhysxCollisionAPI):
+            PhysxSchema.PhysxCollisionAPI.Apply(child_prim)
+        collision_api = UsdPhysics.CollisionAPI(child_prim)
+        collision_api.CreateCollisionEnabledAttr().Set(True)
 
-        api = physx_utils.MESH_APPROXIMATIONS.get(
-            cfg.mesh_approximation, 0
-        )  # None is a valid value
-        if api == 0:
-            carb.log_warn(
-                f"setCollider: invalid approximation type {cfg.mesh_approximation} provided for {prim.GetPath()}. Falling back to convexHull."
-            )
-            cfg.mesh_approximation = "convexHull"
-            api = physx_utils.MESH_APPROXIMATIONS[cfg.mesh_approximation]
-        approximation_api = api.Apply(prim) if api is not None else None
+        if api := physx_utils.MESH_APPROXIMATIONS.get(cfg.mesh_approximation, None):
+            approximation_api = api.Apply(child_prim) if api is not None else None
         if cfg.mesh_approximation == "sdf" and cfg.sdf_resolution:
             approximation_api.CreateSdfResolutionAttr().Set(cfg.sdf_resolution)  # type: ignore
 
-        meshcollisionAPI = UsdPhysics.MeshCollisionAPI.Apply(prim)  # type: ignore
-        meshcollisionAPI.CreateApproximationAttr().Set(cfg.mesh_approximation)
+        mesh_collision_api = UsdPhysics.MeshCollisionAPI.Apply(child_prim)  # type: ignore
+        mesh_collision_api.CreateApproximationAttr().Set(cfg.mesh_approximation)
 
     return True
+
+
+def is_part_of_rigid_body(prim: Usd.Prim) -> bool:
+    if prim.HasAPI(UsdPhysics.RigidBodyAPI):  # type: ignore
+        return True
+    prim = prim.GetParent()
+    return is_part_of_rigid_body(prim) if prim.IsValid() else False
--- simforge/integrations/isaaclab/spawner/from_files/impl.py
+++ simforge/integrations/isaaclab/spawner/from_files/impl.py
@@ -6,7 +6,7 @@ from isaaclab.sim import clone
 from isaaclab.sim.spawners.from_files.from_files import (
     spawn_from_usd as __spawn_from_usd,
 )
-from pxr import PhysxSchema, Usd, UsdPhysics  # type: ignore
+from pxr import PhysxSchema, Usd, UsdGeom, UsdPhysics
 
 if TYPE_CHECKING:
     from simforge.integrations.isaaclab.spawner.from_files.cfg import (
@@ -31,10 +31,9 @@ def spawn_from_usd(
             orientation=orientation,
             scale=cfg.scale,
         )
-    prim: Usd.Prim = stage_utils.get_current_stage().GetPrimAtPath(prim_path)
 
     # Apply missing APIs
-    __apply_missing_apis(prim, cfg)
+    __apply_missing_apis(prim_path, cfg)
 
     # Apply mesh collision API and properties
     if cfg.mesh_collision_props is not None:
@@ -43,29 +42,60 @@ def spawn_from_usd(
     return __spawn_from_usd(prim_path, cfg, translation, orientation)
 
 
-def __apply_missing_apis(prim: Usd.Prim, cfg: "FileCfg"):
-    if cfg.mass_props is not None and not UsdPhysics.MassAPI(prim):  # type: ignore
-        UsdPhysics.MassAPI.Apply(prim)  # type: ignore
+def __apply_missing_apis(prim_path: str, cfg: "FileCfg"):
+    parent_prim: Usd.Prim = stage_utils.get_current_stage().GetPrimAtPath(prim_path)
 
-    if cfg.rigid_props is not None and not UsdPhysics.RigidBodyAPI(prim):  # type: ignore
-        UsdPhysics.RigidBodyAPI.Apply(prim)  # type: ignore
-
-    if cfg.collision_props is not None and not UsdPhysics.CollisionAPI(prim):  # type: ignore
-        UsdPhysics.CollisionAPI.Apply(prim)  # type: ignore
-
-    if cfg.deformable_props is not None and not PhysxSchema.PhysxDeformableBodyAPI(
-        prim
-    ):
-        PhysxSchema.PhysxDeformableBodyAPI.Apply(prim)
-
-    if cfg.articulation_props is not None and not UsdPhysics.ArticulationRootAPI(prim):  # type: ignore
-        UsdPhysics.ArticulationRootAPI.Apply(prim)  # type: ignore
+    if cfg.articulation_props is not None:
+        articulation_root_prim: Usd.Prim | None = None
+        _queue = [parent_prim]
+        while _queue:
+            child_prim = _queue.pop(0)
+            if child_prim.HasAPI(UsdPhysics.ArticulationRootAPI):
+                articulation_root_prim = child_prim
+                break
+            _queue.extend(child_prim.GetChildren())
+        if articulation_root_prim is None:
+            UsdPhysics.ArticulationRootAPI.Apply(parent_prim)
 
     if cfg.fixed_tendons_props is not None:
-        if not PhysxSchema.PhysxTendonAxisAPI(prim):
-            PhysxSchema.PhysxTendonAxisAPI.Apply(prim)
-        if not PhysxSchema.PhysxTendonAxisRootAPI(prim):
-            PhysxSchema.PhysxTendonAxisRootAPI.Apply(prim)
+        fixed_tendon_root_prim: Usd.Prim | None = None
+        _queue = [parent_prim]
+        while _queue:
+            child_prim = _queue.pop(0)
+            if child_prim.HasAPI(PhysxSchema.PhysxTendonAxisRootAPI):
+                fixed_tendon_root_prim = child_prim
+                break
+            _queue.extend(child_prim.GetChildren())
+        if fixed_tendon_root_prim is None:
+            PhysxSchema.PhysxTendonAxisRootAPI.Apply(parent_prim)
 
-    if cfg.joint_drive_props is not None and not UsdPhysics.DriveAPI(prim):  # type: ignore
-        UsdPhysics.DriveAPI.Apply(prim)  # type: ignore
+    for child_prim in parent_prim.GetChildren():
+        if (
+            child_prim.IsA(UsdGeom.Xform)
+            or child_prim.IsA(UsdGeom.Mesh)
+            or child_prim.IsA(UsdGeom.Gprim)
+        ):
+            if cfg.collision_props is not None and not child_prim.HasAPI(
+                UsdPhysics.CollisionAPI
+            ):
+                UsdPhysics.CollisionAPI.Apply(child_prim)
+
+            if cfg.rigid_props is not None and not child_prim.HasAPI(
+                UsdPhysics.RigidBodyAPI
+            ):
+                UsdPhysics.RigidBodyAPI.Apply(child_prim)
+
+            if cfg.mass_props is not None and not child_prim.HasAPI(UsdPhysics.MassAPI):
+                UsdPhysics.MassAPI.Apply(child_prim)
+
+            if cfg.deformable_props is not None and not child_prim.HasAPI(
+                PhysxSchema.PhysxDeformableBodyAPI
+            ):
+                PhysxSchema.PhysxDeformableBodyAPI.Apply(child_prim)
+        elif child_prim.IsA(UsdPhysics.Joint) and not child_prim.IsA(
+            UsdPhysics.FixedJoint
+        ):
+            if cfg.joint_drive_props is not None and not child_prim.HasAPI(
+                UsdPhysics.DriveAPI
+            ):
+                UsdPhysics.DriveAPI.Apply(child_prim)
